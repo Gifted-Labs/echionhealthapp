ebde5ee33e0885182159ea20793e507db5a4fcee
Gifted-Labs <114094639+Gifted-Labs@users.noreply.github.com>
2026-02-11 19:08:28 +0000

    Update OpenApiConfig.java

--- src/main/java/com/giftedlabs/echoinhealthbackend/config/OpenApiConfig.java
+++ src/main/java/com/giftedlabs/echoinhealthbackend/config/OpenApiConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Echoin Health API", version = "1.0", description = "HIPAA-Compliant Ultrasound Report Management System API", contact = @Contact(name = "Echoin Health", email = "support@echoinhealth.com")), servers = {
         @Server(url = "http://localhost:8080/api", description = "Local Development Server"),
-        @Server(url = "https://api.echoinhealth.com/api", description = "Production Server")
+        @Server(url = "https://echionhealthapp.onrender.com/api", description = "Production Server")
 })
 @SecurityScheme(name = "Bearer Authentication", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer", in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {
